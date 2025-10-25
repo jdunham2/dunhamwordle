6bec723c8a2f55be5886a34cca952cdbd518ad71
Jeshua Dunham <jeshuad@gmail.com>
2025-10-24 20:58:14 -0400

    fix icons not showing fully

--- App.tsx
+++ App.tsx
@@ -691,7 +691,7 @@ const createIcon = (svg: React.ReactNode) => (props: React.SVGProps<SVGSVGElemen
         xmlns="http://www.w3.org/2000/svg"
         width="24"
         height="24"
-        viewBox="0 0 24"
+        viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
         strokeWidth="2"
